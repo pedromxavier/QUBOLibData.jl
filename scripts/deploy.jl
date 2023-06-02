5b0e5a987b6bc67d548d506e1bd158458c1bf823
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2023-06-02 15:34:10 -0300

    Add deploy script

---
+++ scripts/deploy.jl
@@ -0,0 +1,45 @@
+using Tar
+using UUIDs
+
+include("_util.jl")
+
+function main(; verbose::Bool = false)
+    datapath = mkpath(joinpath(ROOT_PATH, "dist"))
+
+    for path in listdirs(DATA_PATH)
+        filepath = Tar.create(path) |> abspath
+
+        run(`gzip -9 $filepath`)
+
+        code = basename(path)
+
+        gzippath = joinpath(datapath, "$code.tar.gz")
+
+        cp("$filepath.gz", gzippath)
+
+        rm("$filepath.gz"; force=true)
+
+        @info "Compressed tarball @ $(gzippath)"
+    end
+
+    branch = uuid4()
+
+    cd(ROOT_PATH) do
+        run(`git checkout --orphan $branch`)
+        run(`git rm -rf .`)
+        run(`git add .gitignore LICENSE ./dist/*`)
+        run(`git push --set-upstream origin $branch`)
+
+        @info "Deployed './dist' folder @ '$branch'"
+    end
+
+    return nothing
+end
+
+if "--run" ∈ ARGS
+    if "--verbose" ∈ ARGS
+        main(; verbose=true)
+    else
+        main()
+    end
+end
