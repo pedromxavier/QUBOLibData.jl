6f48089e60db56ad3705d3341d202e6514211ce1
pedromxavier <pedromxavier@poli.ufrj.br>
2023-12-06 16:13:41 -0500

    Update info messages [deploy]

--- deployment/script.jl
+++ deployment/script.jl
@@ -10,10 +10,19 @@ let index = QUBOInstances.create_index(abspath(@__DIR__, ".."))
     @info "Root path: $(index.root_path)"
     @info "Dist path: $(index.dist_path)"
 
+    @info "Curating benchmark data..."
     QUBOInstances.curate!(index)
+
+    @info "Computing tree hash..."
     QUBOInstances.hash!(index)
+
+    @info "Generating distribution..."
     QUBOInstances.deploy!(index)
 
+    @info "Updating environment variables..."
     ENV["GIT_TREE_HASH"]    = index.tree_hash[]::String
     ENV["NEXT_QUBOLIB_TAG"] = index.next_tag[]::String
+
+    @info ENV["GIT_TREE_HASH"]
+    @info ENV["NEXT_QUBOLIB_TAG"]
 end
